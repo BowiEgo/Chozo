6d0fe76d2224884379fb9f74299cc84156c7401a
bowiego <295030485@qq.com>
2024-05-23 02:01:27 +0800

    style: Add colorful hint to the shell script for installing.

--- install.sh
+++ install.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+MAGENTA='\033[1;35m'
+CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+NC='\033[0m'
+
+export PS4="${CYAN}-- ${NC}\$(date \"+%Y-%m-%d %H:%M:%S\")${CYAN} --[execute bash command]: \${FUNCNAME[0]:+\${FUNCNAME[0]}: }${NC}"
+
+set -x
+
 git submodule update --init --recursive
+{
+  set +x
+} 2>/dev/null
 echo "Submodules have been initialized and updated."
+
+set -x
 
 ./tools/vcpkg/bootstrap-vcpkg.sh
 ./tools/vcpkg/vcpkg install
@@ -9,3 +27,7 @@ echo "Submodules have been initialized and updated."
 mkdir build
 cmake -S . -B build
 cmake --build build
+
+{
+  set +x
+} 2>/dev/null
