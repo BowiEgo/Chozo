c79a5ffd5ee219f1ccf1d7bea5225ee70280e982
bowiego <295030485@qq.com>
2024-08-03 20:23:54 +0800

    fix: enable depth test.

--- ChozoEngine/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ ChozoEngine/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -7,6 +7,8 @@ namespace Chozo {
     {
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        glEnable(GL_DEPTH_TEST);
     }
 
     int OpenGLRendererAPI::GetMaxTextureSlots()
