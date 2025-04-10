220df149a12c7d5a141a63acd2930a389d4aa090
boboboost <295030485@>
2025-04-10 18:13:54 +0800

    feat: Add imgui.ini to bin directory.

---
+++ cmake/ResourceUtils.cmake
@@ -0,0 +1,63 @@
+# ========== Resource Copy Utilities ==========
+
+function(define_target_resource_path TARGET MACRO_NAME RELATIVE_PATH)
+    target_compile_definitions(${TARGET} PRIVATE ${MACRO_NAME}="${RELATIVE_PATH}")
+endfunction()
+
+function(copy_file_to_target TARGET FROM_FILE TO_NAME)
+    add_custom_command(TARGET ${TARGET} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy
+            ${FROM_FILE}
+            $<TARGET_FILE_DIR:${TARGET}>/${TO_NAME}
+            COMMENT "Copying file ${TO_NAME} to target output dir"
+    )
+endfunction()
+
+function(copy_file_to_parent TARGET FROM_FILE TO_NAME)
+    add_custom_command(TARGET ${TARGET} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy
+            ${FROM_FILE}
+            $<TARGET_FILE_DIR:${TARGET}>/../${TO_NAME}
+            COMMENT "Copying file ${TO_NAME} to target output parent dir"
+    )
+endfunction()
+
+function(copy_directory_to_target TARGET FROM_DIR TO_NAME)
+    add_custom_command(TARGET ${TARGET} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy_directory
+            ${FROM_DIR}
+            $<TARGET_FILE_DIR:${TARGET}>/${TO_NAME}
+            COMMENT "Copying directory ${TO_NAME} to target output dir"
+    )
+endfunction()
+
+function(copy_directory_to_parent TARGET FROM_DIR TO_NAME)
+    add_custom_command(TARGET ${TARGET} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy_directory
+            ${FROM_DIR}
+            $<TARGET_FILE_DIR:${TARGET}>/../${TO_NAME}
+            COMMENT "Copying directory ${TO_NAME} to target output parent dir"
+    )
+endfunction()
+
+function(copy_platform_imgui_ini TARGET)
+    if(WIN32)
+        set(PLATFORM "Windows")
+    elseif(APPLE)
+        set(PLATFORM "Mac")
+    else()
+        message(WARNING "Unsupported platform for imgui.ini copy.")
+        return()
+    endif()
+
+    set(SOURCE_PATH "${CMAKE_SOURCE_DIR}/ChozoEditor/settings/${PLATFORM}/imgui.ini")
+
+    add_custom_command(TARGET ${TARGET} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy
+            ${SOURCE_PATH}
+            $<TARGET_FILE_DIR:${TARGET}>/imgui.ini
+            COMMENT "Copying imgui.ini for platform: ${PLATFORM}"
+    )
+endfunction()
+
+message(STATUS "[ResourceUtils] Resource copy functions loaded.")
